fd8dfc91a56857612f5f8e6b83d51ec66557569e
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-11-08 04:03:28 +0600

    footer-added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,17 +7,17 @@ const Footer = () => {
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <div>
                     <span className="footer-title">Services</span> 
-                    <Link className="link link-hover">Branding</Link> 
-                    <Link className="link link-hover">Design</Link> 
-                    <Link className="link link-hover">Marketing</Link> 
-                    <Link className="link link-hover">Advertisement</Link>
+                    <Link className="link link-hover">Appointment</Link> 
+                    <Link className="link link-hover">Medicine</Link> 
+                    <Link className="link link-hover">Check Up</Link> 
+                    <Link className="link link-hover">Support</Link>
                 </div> 
                 <div>
-                    <span className="footer-title">Company</span> 
-                    <Link className="link link-hover">About us</Link> 
+                    <span className="footer-title">More Info</span> 
+                    <Link className="link link-hover">About me</Link> 
                     <Link className="link link-hover">Contact</Link> 
-                    <Link className="link link-hover">Jobs</Link> 
-                    <Link className="link link-hover">Press kit</Link>
+                    <Link className="link link-hover">How it works</Link> 
+                    <Link className="link link-hover">Data & Research</Link>
                 </div> 
                 <div>
                     <span className="footer-title">Legal</span> 
@@ -25,11 +25,22 @@ const Footer = () => {
                     <Link className="link link-hover">Privacy policy</Link> 
                     <Link className="link link-hover">Cookie policy</Link>
                 </div>
+                <div>
+                    <span className="footer-title">Download App</span> 
+                    <div className='w-32 rounded-lg'>
+                        <img src="https://i.ibb.co/jDVp92j/playstore.jpg" alt="" />
+                    </div>
+                    <div className='w-32 rounded-lg'>
+                        <img src="https://i.ibb.co/3khz8CJ/appStore.jpg" alt="" />
+                    </div>
+                </div>
             </footer> 
             <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
                 <div className="items-center grid-flow-col">
-                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-                    <p>ACME Industries Ltd. <br/>Providing reliable tech since 1992</p>
+                    <div className="w-10 rounded-full mr-1">
+                        <img src="https://i.ibb.co/vVT97rV/doctor-g-png.jpg" alt=''/>
+                    </div>
+                    <p className='font-bold'><span className='text-rose-900'>Doctor</span><span className='text-green-900 ml-1'>G</span><br/>Providing reliable health services since 2010</p>
                 </div> 
                 <div className="md:place-self-center md:justify-self-end">
                     <div className="grid grid-flow-col gap-4">
@@ -37,6 +48,11 @@ const Footer = () => {
                     <Link><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></Link>
                     <Link><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></Link>
                     </div>
+                </div>
+            </footer>
+            <footer className="footer footer-center px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+                <div>
+                    <p>Copyright © 2010 - All right reserved by <span className='text-rose-900 font-bold'>Doctor</span><span className='text-green-900 ml-1 font-bold'>G</span></p>
                 </div>
             </footer>
         </div>
